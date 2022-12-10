360bc09ab915006ed04cb7a393204c8ea9fb37be
Овчинников Игорь <faceplaces@inbox.ru>
2022-12-10 08:24:09 +0300

    Django testing

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,6 +84,8 @@ DATABASES = {
         'NAME': 'netology_django_testing',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres'
     }
 }
 
@@ -115,7 +118,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+# USE_L10N = True
 
 USE_TZ = True
 
@@ -124,3 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
+
+MAX_STUDENTS_PER_COURSE = 20
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,130 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from rest_framework.test import APIClient
+from model_bakery import baker
+from students.models import Student, Course
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def route():
+    return '/api/v1/courses/'
+
+
+@pytest.fixture
+def student_factory():
+    def student(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return student
+
+
+@pytest.fixture
+def course_factory():
+    def course(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+
+    return course
+
+
+@pytest.mark.django_db
+def test_get_course(client, route, student_factory, course_factory):
+    """Get 1 course test"""
+    # Arrange
+    course = course_factory(_quantity=1)
+
+    # Act
+    response = client.get(route)
+
+    # Assert
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == len(course)
+    assert data[0]['name'] == course[0].name
+
+
+@pytest.mark.django_db
+def test_get_courses(client, route, student_factory, course_factory):
+    """Get list of courses test"""
+    # Arrange
+    courses = course_factory(_quantity=10)
+
+    # Act
+    response = client.get(route)
+
+    # Assert
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == len(courses)
+    for index, course in enumerate(data):
+        assert course['name'] == courses[index].name
+
+
+@pytest.mark.django_db
+def test_course_filter_by_id(client, route, student_factory, course_factory):
+    """Filtering corses by id"""
+    courses = course_factory(_quantity=2)
+
+    response = client.get(f'{route}?id={courses[0].pk}')
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]['id'] == courses[0].pk
+
+
+@pytest.mark.django_db
+def test_course_filter_by_name(client, route, student_factory, course_factory):
+    """Filtering corses by name"""
+    courses = course_factory(_quantity=2)
+
+    response = client.get(f'{route}?name={courses[0].name}')
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]['name'] == courses[0].name
+
+
+@pytest.mark.django_db
+def test_create_course(client, route, student_factory):
+    """Create course"""
+    count_courses = Course.objects.count()
+
+    response = client.post(route, data={'name': 'test_course'})
+
+    assert response.status_code == 201
+    assert Course.objects.count() == count_courses + 1
+
+
+@pytest.mark.django_db
+def test_update_course(client, route, student_factory, course_factory):
+    """Update course"""
+    course = course_factory(_quantity=1)
+
+    response = client.patch(f'{route}{course[0].pk}/', data={'name': 'test_course'})
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data['name'] == 'test_course'
+
+
+@pytest.mark.django_db
+def test_delete_course(client, route, student_factory, course_factory):
+    """Delete course"""
+    course = course_factory(_quantity=1)
+    course_count = Course.objects.count()
+
+    response = client.delete(f'{route}{course[0].pk}/')
+
+    assert response.status_code == 204
+    assert Course.objects.count() == course_count - 1
+
+
+@pytest.mark.parametrize("test_input, expected", [(2, 2), (20, 20), (10, 20)])
+def test_max_students_per_course(settings, test_input, expected):
+    """Max students per course"""
+    settings.MAX_STUDENTS_PER_COURSE = test_input
+
+    assert settings.MAX_STUDENTS_PER_COURSE == expected
